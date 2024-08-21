a8d6278b707d6a3bf85759fd1fae4ff2d87268b8
Nischay <barejanischay5@gmail.com>
2024-08-21 17:35:54 +0530

    node basics

---
+++ JavascriptBasics/script.js
@@ -0,0 +1,35 @@
+// alert("")
+
+var arr = [1, 2, 3, 4];
+
+
+//foreach
+
+// arr.forEach(function (val) {
+//     console.log(val + "hello");
+// })
+
+//maps
+
+// var ans = arr.map(function (val) {
+//     return val + 1;
+// })
+// console.log(ans);
+
+//filter
+
+// var ans = arr.filter(function (val) {
+//     if (val > 3) return true;
+//     else return false;
+// })
+
+// console.log(ans);
+
+//find
+
+// var ans = arr.find(function (val) {
+//     if (val == 2) return val;
+// })
+
+// console.log(ans);
+
---
+++ nodeJsBasics/script.js
@@ -0,0 +1,49 @@
+// file System operations
+
+// const fs = require('fs');
+
+
+// fs.writeFile("hey.txt", "hey hello how are you", function (err) {
+//     if (err) console.error(err);
+//     else console.log("done");
+// })
+
+// fs.appendFile("hey.txt", "i am good you tell", function (err) {
+//     if (err) console.error(err);
+//     else console.log("done");
+// })
+
+// fs.rename("hey.txt", "hi.txt", function (err) {
+//     if (err) console.log(err);
+//     else console.log("done");
+// })
+
+// fs.copyFile("hi.txt", "./copy/copy.txt", function (err) {
+//     if (err) console.log(err.message);
+//     else console.log("done");
+// })
+
+// fs.unlink("hi.txt", function (err) {
+//     if (err) console.log(err);
+//     else console.log("removed");
+// })
+
+// fs.rmdir("./copy", { recursive: true }, function (err) {
+//     if (err) console.error(err);
+//     else console.log("removed");
+// })
+
+// we can also use fs.rm
+
+// HTTP module
+
+// const http = require('http');
+
+// const server = http.createServer(function (req, res) {
+//     res.end("hello world");
+// })
+
+// server.listen(3000);
+
+
+
